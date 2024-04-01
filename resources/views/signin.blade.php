0d693d34e38d56ed337615f40eee02d4037438ca
QuardSA <smirnov_aleksey04@inbox.ru>
2024-04-01 09:49:48 +0500

    add reg/auth form

---
+++ resources/views/signin.blade.php
@@ -0,0 +1,36 @@
+<x-links></x-links>
+<div class="form_registration" id="form_registration">
+	<div class="form-container sign-up-container">
+		<form action="#" class="auth_reg">
+			<h1 class="fw-bold">Создайте аккаунт</h1>
+			<input class="border-0 px-1 py-2 w-100" style="background-color: #eee" type="text" placeholder="Имя" name="name"/>
+			<input class="border-0 px-1 py-2 w-100" style="background-color: #eee" type="email" placeholder="Почта" name="email"/>
+			<input class="border-0 px-1 py-2 w-100" style="background-color: #eee" type="password" placeholder="Пароль" name="password"/>
+            <input class="border-0 px-1 py-2 w-100" style="background-color: #eee" type="password" placeholder="Повторите пароль" name="password_reqired"/>
+			<button class="border rounded-pill bg-danger border-danger text-white px-4 py-2 fs-6 fw-bold">Зарегистрироваться</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#" class="auth_reg">
+			<h1 class="fw-bold">Авторизируйтесь</h1>
+			<input class="border-0 px-1 py-2 w-100" style="background-color: #eee" type="email" placeholder="Почта" name="email"/>
+			<input class="border-0 px-1 py-2 w-100" style="background-color: #eee" type="password" placeholder="Пароль" name="password" />
+			<button class="border rounded-pill bg-danger border-danger text-white px-4 py-2 fs-6 fw-bold">Авторизоваться</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Вы вернулись!</h1>
+				<p>Что-бы зайти в свой аккаунт введите свою почту и пароль</p>
+				<button class="ghost border rounded-pill border-white px-4 py-1 text-white" id="signIn">Войти</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Добро пожаловать!</h1>
+				<p>Введите свои персональные данные и присоединяйтесь к нам</p>
+				<button class="ghost border rounded-pill border-white px-4 py-1 text-white" id="signUp">Зарегистрироваться</button>
+			</div>
+		</div>
+	</div>
+</div>
+<x-scripts></x-scripts>
